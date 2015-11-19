37e4401d132d3e7d9cb2351b21bb2a8746351156
aakarshgoel96 <aakarshgoel96@gmail.com>
2015-11-19 16:09:04 +0530

    json files added

---
+++ jsonprimary.php
@@ -0,0 +1,16 @@
+<?php 
+$host = "localhost"; 
+$user = "root"; 
+$pass = ""; 
+$database = "healthcare"; 
+
+$linkID = mysql_connect($host, $user, $pass) or die("Could not connect to host."); 
+mysql_select_db($database, $linkID) or die("Could not find database."); 
+
+$query = mysql_query("SELECT * FROM user_primary");
+$rows = array();
+while($row = mysql_fetch_assoc($query)) {
+    $rows[] = $row;
+}
+print json_encode($rows);
+?>
