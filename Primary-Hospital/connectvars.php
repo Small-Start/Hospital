340d316264ede26fbf087174625057917a308448
aayusharora <aayush113002@gmail.com>
2016-10-27 23:27:40 +0530

    Cleaning 150mb pack file

--- Primary-Hospital/connectvars.php
+++
@@ -1,8 +0,0 @@
-<?php
-  // Define database connection constants
-  define('DB_HOST', 'localhost');
-  define('DB_USER', 'root');
-  define('DB_PASSWORD', '');
-  define('DB_NAME', 'healthcare');
-  define('HOST','Hospital');
-?>
